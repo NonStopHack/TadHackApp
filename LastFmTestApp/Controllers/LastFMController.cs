8279ad42adbfe370f32cbce61598195f0754128a
wadim1611 <vad1611@yandex.ru>
2017-09-23 17:15:31 +0300

    first commit

---
+++ LastFmTestApp/Controllers/LastFMController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IF.Lastfm.Core.Objects;
+
+namespace LastFmTestApp.Controllers
+{
+    public class LastFMController : Controller
+    {
+        [Authorize]
+        public ActionResult LastFmProfile()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Playlists()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Playlists(string playlistId)
+        {
+            return View();
+        }
+        
+        [Authorize]
+        public ActionResult Album(string playlistId)
+        {
+            var response = client.Album.GetInfoAsync("Grimes", "Visions");
+            LastAlbum visions = response.Content;
+            return View();
+        }
+    }
+}
---
+++ LastFmTestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LastFmTestApp.Startup))]
+namespace LastFmTestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
